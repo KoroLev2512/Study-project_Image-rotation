6071cf9b6c1f69c3f6a186d46e1c8c988dc5aeaa
Yurii Korolev <yurii@niuitmo.ru>
2021-12-31 15:39:20 +0000

    Upload New File

---
+++ solution/include/bmp_header.h
@@ -0,0 +1,28 @@
+#ifndef SOLUTION_BMP_HEADER_H
+#define SOLUTION_BMP_HEADER_H
+
+#include "image.h"
+
+#define BF_TYPE 0x4D42
+
+#pragma pack(push, 1)
+struct bmp_header {
+    uint16_t bfType;
+    uint32_t bfileSize;
+    uint32_t bfReserved;
+    uint32_t bOffBits;
+    uint32_t biSize;
+    uint32_t biWidth;
+    uint32_t biHeight;
+    uint16_t biPlanes;
+    uint16_t biBitCount;
+    uint32_t biCompression;
+    uint32_t biSizeImage;
+    uint32_t biXPelsPerMeter;
+    uint32_t biYPelsPerMeter;
+    uint32_t biClrUsed;
+    uint32_t biClrImportant;
+};
+#pragma pack(pop)
+
+#endif
